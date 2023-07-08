029a38bf92b3f4c519df4a88bd6613a3a0105893
Kimiya <kimia.goudarzi81@gmail.com>
2023-07-09 00:41:04 +0330

    Change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,13 +38,11 @@ const NavBar = () => {
               <div className="flex flex-1 items-center justify-center sm:items-stretch">
                 <div className="hidden flex sm:ml-6 sm:block">
                   <div className="flex space-x-9">
-                      {/* <button type="button" className="rounded-full bg-[#FFFFFF] relative z-40 ml-6 my-1 p-1.5 text-[#FFFFFF] text-[14px] hover:text-white focus:outline-none focus:ring-1 focus:ring-#4385F6 focus:ring-offset-1 Pl-5 focus:ring-offset-[#4385F6]"> */}
                         <button type="button" className="rounded-full text-[#4385F6] absolute z-40 ml-6 my-1 p-1.5 bg-[#FFFFFF] text-[14px] hover:text-white focus:outline-none focus:ring-1 focus:ring-#4385F6 focus:ring-offset-1 Pl-5 focus:ring-offset-[#4385F6]">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                               <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                             </svg>
                         </button>
-                      {/* </button> */}
                     <button className="rounded-full bg-[#4385F6] font-[700] text-[16px] px-11 py-3 relative text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-[#4385F6] focus:ring-offset-2 focus:ring-offset-[#4385F6]">ورود و ثبت نام</button>
                     <a href="#" className="text-[#212121] font-[400] rounded-md px-3 py-2 text-[14px]">بلاگ</a>
                     <a href="#" className="text-[#212121] font-[400] rounded-md px-3 py-2 text-[14px]">همکاری با ما</a>
